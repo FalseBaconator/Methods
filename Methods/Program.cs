ce815357727bd05f4c3ad84520169421f8fb2509
FalseBaconator <113189775+FalseBaconator@users.noreply.github.com>
2022-10-03 12:17:06 -0300

    Changed the likelyhood of different updates.

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -18,7 +18,7 @@ namespace Methods
         static int HP = 100;
         static int HPMin = 0;
         static int HPMax = 100;
-        static int enemyDMG = 10;
+        static int enemyDMG = -10;
         static int healthKit = 25;
         static int lives = 3;
         static int loseLife = 1;
@@ -27,29 +27,11 @@ namespace Methods
         static float shieldDMGReduce = 3.0f;
         static float shieldMin = 0.0f;
         static float shieldFull = 10.0f;
-        static int lavaDMG = 100;
+        static int lavaDMG = -1000;
 
         static void Main(string[] args)
         {
-            studio = "Fake Bacon Games";
-            title = "Not A Real Game";
-            score = 0;
-            enemyScore = 10;
-            scoreMultiplier = 1;
-            scoreMultiplyItem = 2;
-            HP = 100;
-            HPMin = 0;
-            HPMax = 100;
-            enemyDMG = -10;
-            healthKit = 25;
-            lives = 3;
-            loseLife = -1;
-            oneUp = 1;
-            shield = 0.0f;
-            shieldDMGReduce = 3.0f;
-            shieldMin = 0.0f;
-            shieldFull = 10.0f;
-            lavaDMG = -100;
+            Respawn();
 
             ShowHud();
             RandomizedUpdate();
@@ -63,8 +45,8 @@ namespace Methods
             RandomizedUpdate();
             RandomizedUpdate();
             Update(0, lavaDMG, 0, 0, 0);
+            Update(0, lavaDMG, 0, 0, 0);
 
-            Console.ReadKey(true);
         }
 
 
@@ -90,42 +72,35 @@ namespace Methods
             shieldFull = 10.0f;
             lavaDMG = -1000;
 
-            Console.Clear();
-
-            Console.WriteLine(title + " by " + studio);
-            Console.WriteLine();
-            Console.WriteLine("You Died!");
-            Console.WriteLine("Prepare for respawn");
-
-            Console.ReadKey(true);
+            
         }
 
 
         static void RandomizedUpdate()
         {
             Random rand = new Random();
-            int check = rand.Next(1, 7);
-            if (check == 1)
+            int check = rand.Next(1, 21);
+            if (check >= 1 && check <= 10)
             {
                 //player kills enemy
                 Update(enemyScore, 0, 0, 0, 0);
-            }else if (check == 2)
+            }else if (check == 11)
             {
                 //player gains health kit
                 Update(0, healthKit, 0, 0, 0);
-            }else if (check == 3)
+            }else if (check >= 12 && check <= 17)
             {
                 //player gets attacked
                 Update(0, enemyDMG, 0, 0, 0);
-            }else if (check == 4)
+            }else if (check == 18)
             {
                 //player finds shield
-                Update(0, 0, shieldFull, 0, 0);
-            }else if (check == 5)
+                //Update(0, 0, shieldFull, 0, 0);
+            }else if (check == 19)
             {
                 //player finds 1up
                 Update(0, 0, 0, oneUp, 0);
-            }else if (check == 6)
+            }else if (check == 20)
             {
                 //player finds multiplier
                 Update(0, 0, 0, 0, scoreMultiplyItem);
@@ -158,6 +133,14 @@ namespace Methods
             if (HP <= 0)
             {
                 lives = lives - 1;
+                Console.Clear();
+
+                Console.WriteLine(title + " by " + studio);
+                Console.WriteLine();
+                Console.WriteLine("You Died!");
+                Console.WriteLine("Prepare for respawn");
+
+                Console.ReadKey(true);
                 Respawn();
             }
             ShowHud();
